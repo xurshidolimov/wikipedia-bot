ae089dfd251e8d3d67ed1198a7410d7b8888b250
SX Computers <xurshidolimov017@gmail.comgit config --global user.name>
2022-06-05 15:04:32 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import logging
+import wikipedia
+from aiogram import Bot, Dispatcher, executor, types
+
+#xurshidolimovbot
+
+API_TOKEN = '1903110915:AAGSo9CkqwM1BPD4LHi5JhNtgGe5c-Rjr78'
+wikipedia.set_lang("uz")
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Salom wikipedia botga xush kelibsiz!")
+
+
+
+@dp.message_handler()
+async def sendwiki(message: types.Message):
+    
+    try:
+        respond=wikipedia.summary(message.text)
+        await message.answer(respond)
+ 
+    except:
+        await message.answer("Bu mavzuga oid maqola topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
